dd0161983a8793b6ad60f440edb50d8198aba78e
Raffael <github@raffaeljesche.de>
2018-10-28 22:48:45 +0100

    moved renderer into .tag file

--- ModuleLink/admin.php
+++ ModuleLink/admin.php
@@ -2,7 +2,6 @@
 
 $app->on('admin.init', function() use($app){
     
-    $this->helper('admin')->addAssets('modulelink:assets/component.js');
     $this->helper('admin')->addAssets('modulelink:assets/components/field-module-link.tag');
     
     $this->bindClass('ModuleLink\\Controller\\Admin', 'modulelink');
