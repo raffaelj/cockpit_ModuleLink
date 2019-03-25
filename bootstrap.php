3fad51d90e2faa45b687e8d94ff3872c4428bdb8
Raffael <github@raffaeljesche.de>
2019-03-25 18:08:09 +0100

    added snippet to prevent wrong folder name

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,29 @@
 <?php
+/**
+ * ModuleLink Field for Cockpit CMS
+ * 
+ * @see       https://github.com/raffaelj/cockpit_ModuleLink/
+ * @see       https://github.com/agentejo/cockpit/
+ * 
+ * @version   0.1.1
+ * @author    Raffael Jesche
+ * @license   MIT
+ * @note      work in progress
+ */
+
+// controller autoload and path short codes need a folder naming pattern
+// return if addon folder has wrong name, e. g. "cockpit_AddonName"
+$name = 'ModuleLink';
+
+if (!isset($app['modules'][strtolower($name)])) {
+
+    // display a warning on top of admin ui
+    $app->on('app.layout.contentbefore', function() use ($name) {
+        echo '<p><span class="uk-badge uk-badge-warning"><i class="uk-margin-small-right uk-icon-warning"></i>' . $name . '</span> You have to rename the addon folder <code>' . basename(__DIR__) . '</code> to <code>' . $name . '</code>.</p>';
+    });
+
+    return;
+}
 
 $app->module('collections')->extend([
 
