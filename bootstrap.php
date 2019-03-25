401346d77ef344c9aa993ca4a6b3c4fb39221996
Raffael <github@raffaeljesche.de>
2019-03-25 18:01:13 +0100

    moved all addon files to root

--- bootstrap.php
+++ bootstrap.php
@@ -1,20 +1,75 @@
 <?php
 
-/*
- * This is a dummy bootstrap file to prevent a
- * `Fatal error: require(): Failed opening required ...`
- * when `\Lime\App` tries to load the module without an
- * existing `bootstrap.php`.
- *
- * Don't copy the whole repository into your addons folder.
- * You only need the one, that is named like this addon.
- *
- * Sorry for the confusion, but I like to keep some data
- * (e. g. .gitignore or a docs folder) in the root, that
- * aren't necessary in production use of the addon.
- *
- */
+$app->module('collections')->extend([
 
-$app->on('app.layout.contentbefore', function(){
-    echo '<p class="uk-panel"><span class="uk-badge uk-badge-warning"><i class="uk-margin-small-right uk-icon-warning"></i>' . basename(__DIR__) . '</span> You copied the wrong addon folder. Have a look at the addon\'s readme file for instructions.</p>';
-});
+    // overwrite original _populate function
+    '_populate' => function($items, $maxlevel = -1, $level = 0, $fieldsFilter = []) {
+
+        if (!is_array($items)) {
+            return $items;
+        }
+
+        // mini hack to populate 1 when option passed via function and param is not available
+        $populate_module = $this->app->param('populate_module') ?? ($maxlevel === false ? 1 : false);
+
+        if (isset($populate_module) && is_numeric($populate_module)) {
+            $items = cockpit_populate_module($items, intval($populate_module), 0, $fieldsFilter);
+        }
+
+        // added a fix to not expand collection-links with invalid "populate":"string"
+        return $maxlevel === false ? $items : cockpit_populate_collection($items, $maxlevel, 0, $fieldsFilter);
+
+    }
+
+]);
+
+$app->module('collections')->extend([
+    'getModuleData' => function($module, $options = []) {
+        return $this->find($module, $options);
+    }
+]);
+
+$app->module('singletons')->extend([
+    'getModuleData' => function($module, $options = []) {
+        return $this->getData($module, $options);
+    }
+]);
+
+$app->module('forms')->extend([
+    'getModuleData' => function($module, $options = []) {
+        return $this->find($module, $options);
+    }
+]);
+
+
+function cockpit_populate_module(&$items, $maxlevel = -1, $level = 0, $fieldsFilter = []) {
+
+    if (!is_array($items)) {
+        return $items;
+    }
+
+    if (is_numeric($maxlevel) && $maxlevel > -1 && $level > ($maxlevel+1)) {
+        return $items;
+    }
+
+    foreach ($items as $k => &$v) {
+
+        if (is_array($items[$k])) {
+            $items[$k] = cockpit_populate_module($items[$k], $maxlevel, ($level + 1), $fieldsFilter);
+        }
+
+        if (isset($v['_id'], $v['module'], $v['name'], $v['display'])) {
+            $link = $v['name'];
+            $items[$k] = cockpit($v['module'])->getModuleData($v['name']);
+            $items[$k]['_modulelink'] = $link;
+            $items[$k] = cockpit_populate_module($items[$k], $maxlevel, $level, $fieldsFilter);
+        }
+    }
+
+    return $items;
+}
+
+// ADMIN
+if (COCKPIT_ADMIN && !COCKPIT_API_REQUEST) {
+    include_once(__DIR__ . '/admin.php');
+}
